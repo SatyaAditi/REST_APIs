e995144375e2f6d0aa2bacaa1ef06d8a4ba807b7
Satya Aditi <satyaaditi93@gmail.com>
2018-09-28 00:55:51 -0700

    to insert data into the database and show data,only problem is when showing data ,it only shows the data in the last row as the response gets updated.Slution might be to use a list but that is creating problem in response

--- test/DataManager.java
+++ test/DataManager.java
@@ -4,24 +4,32 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.MediaType;
+import javax.xml.bind.annotation.XmlRootElement;
 
+//@XmlRootElement
 @Path("/dbconnection")
 public class DataManager {
-	
+	DbManager db= new DbManager();
+	Connection conn=db.getConnection();
+	ArrayList<String> list=new ArrayList<>();
+	@Path("/showdata")
 	@GET
 	
 	@Produces(MediaType.APPLICATION_XML)
 	public String getinfo()
 	{
     String response = null;
-	DbManager db= new DbManager();
-	Connection conn=db.getConnection();
+	
 	Statement stmt;
 	try {
 		stmt = conn.createStatement();
@@ -29,17 +37,43 @@ public class DataManager {
 	ResultSet rs=stmt.executeQuery("select * from user");
 	while(rs.next())
 	{
-	  response="<?xml version=1.0?>"+ "name " + "<name>"+rs.getString(2)+"<name>";
+	  response="<?xml version=1.0?>"+ "name " + "<name>"+ rs.getString(2) +"</name>";
+	  //list.add(response);
+	  
 	 }
 	conn.close();
-	return "hey"+response;
+	//GenericEntity<ArrayList<String>> entity = new GenericEntity<ArrayList<String>>(list) {};
+	return response;
 	}
 	catch (SQLException e) {
 		
 		e.printStackTrace();
-		return null;
+		return response="error";
 	}
 	}
+	
+	@Path("/insertinfo")
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String insertinfo(@QueryParam("id")int id,@QueryParam("name")String username,@QueryParam("password")String password)
+	{
+		String responsenew="";
+		Statement stmt;
+		try {
+		stmt=conn.createStatement();
+		String sql="insert into user(id,username,password) values("+ id + "," + username + "," + password +")";
+		
+			stmt.executeUpdate(sql);
+			conn.close();
+	    return responsenew="updated successfully";
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+			return responsenew="error";
+		}
+		
+
+		}
 	
 	
 }
