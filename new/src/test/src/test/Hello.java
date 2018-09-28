26db55324089fbd18608c0754c99a0bbb489d3e0
Satya Aditi <satyaaditi93@gmail.com>
2018-09-27 17:12:07 -0700

    Second API

---
+++ new/src/test/src/test/Hello.java
@@ -0,0 +1,62 @@
+
+package test;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@Path("/helloaditi")
+public class Hello {
+	@Path("/helloxml/{name}")
+@POST
+@Produces(MediaType.TEXT_XML)
+			public String sayHelloXML(@PathParam("name")String name)
+			{
+			  String resource="<?xml version='1.0'?>" 
+			  		+ "<hello>This is xml " + name+"</hello>";
+			  		
+			  		
+			  		
+			  return resource;
+			}
+	@Path("/hellojson")
+@GET
+@Produces(MediaType.APPLICATION_JSON)
+			public String sayHelloJASON()
+			{
+			  String resource="this is json";
+			  return resource;
+			}
+@GET
+@Produces(MediaType.TEXT_HTML)
+			public String sayHelloHTML()
+			{
+			  String resource="<h1>This is html</h1>";
+			  return resource;
+			}
+
+	@Path("/card")
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String creditCard(@QueryParam("name")String name,@QueryParam("Card_no")String card_no,@QueryParam("amount") int amount)
+	{
+		String response;
+		if(amount>10000)
+		{
+			response="Credit card approved";
+		}
+		else
+		{
+			response="Credit card not approved";
+		}
+		
+		return "<html>"+"<body>"+"<h1>Hello</h1>" + name + "<br>" +
+		response + "</body>" + "</html>";
+	}
+}
