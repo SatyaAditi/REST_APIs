7f04fb09e5998810ac78a2dcec33eedc47ea8faa
Satya Aditi <satyaaditi93@gmail.com>
2018-09-28 23:12:26 -0700

    Use of Modeland View object to display multiple views and model is the data which is givn as input.Previous commit didnt mark any changes.

--- FirstProject/src/main/main/java/com/example/demo/HomeController.java
+++ FirstProject/src/main/main/java/com/example/demo/HomeController.java
@@ -1,22 +1,22 @@
 package com.example.demo;
 
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-
-import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController  {
     
 	@RequestMapping("home")
 	
-	public String home(String name,HttpSession session)
+	public ModelAndView home(@RequestParam("name") String name)
 	{
-		session.setAttribute("name", name);
-		return "HomeView";
+		ModelAndView mv= new ModelAndView();
+		mv.addObject("name",name);
+		mv.setViewName("HomeView");
+		
+		return mv;
 	}
 	
 	
