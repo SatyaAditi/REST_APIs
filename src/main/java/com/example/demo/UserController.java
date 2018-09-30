8ce77df070bdce9bc9a7b4afd0b9c36438707c8e
aditi <satyaaditi93@gmail.com>
2018-09-30 13:50:47 -0700

    PUT and DELETE requests

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -5,9 +5,11 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,7 +21,8 @@ import org.springframework.web.servlet.ModelAndView;
 //Rest controller is used for restful services,the data displayed at client side is either in XML format or JSON format.
 //If we want to restrict to any one type of data,we specify it after path with produces keyword.
 @RestController
-public class UserController {
+public class UserController
+{
    /*Autowire connects 2 objects in the spring ioc container.But in Spring boot the repo object is directly connecte to user object. 
 	As long as you dont associate an interface object with an undefined method,it wont show error.Here we are linking interface object with another object*/
 	@Autowired
@@ -74,4 +77,23 @@ public class UserController {
 		   return repo.findById(id);		   
 	   }
 	   
-}
+	   @DeleteMapping("/user/{id}")
+	   //To delete data using postman
+	   public String deleteUser(@PathVariable int id)
+	   {
+		   User u=repo.getOne(id);
+		   repo.delete(u);
+		   return "deleted successfully";
+	   }
+
+	   //To update data through resquest body
+	   @PutMapping(path="/adduser",consumes= {"application/json"})
+       public User updateorsaveUser(@RequestBody User u)
+       {
+		   repo.save(u);//as it extends crud
+		   //repo directly saves the value in the database as it extends crud repository which does create ,update and stuff.All this is managed by JPA.JPA created the tabel based on the variables in the user class with id as primary key.
+    	   return u;
+    	   //Returning the object of user class and this object is displayed as JSOn or XML.
+       }
+	   
+}	   
